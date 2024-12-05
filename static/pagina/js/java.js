3f33d3bacb9912a2040be16d29ad8570787f9f47
Frvncoo69 <franco.miranda.2107@gmail.com>
2024-12-05 02:45:11 -0300

    listo locooooo

--- static/pagina/js/java.js
+++ static/pagina/js/java.js
@@ -1,21 +1,35 @@
+// Variables globales para controlar el carrusel
+var currentIndex = 0;
+
+function moveCarousel(direction) {
+    var carousel = document.querySelector('.carousel');
+    var totalImages = carousel.children.length;
+    
+    // Actualizar el índice actual con la dirección
+    currentIndex += direction;
+
+    // Si el índice se sale de rango, vuelve al principio o al final
+    if (currentIndex < 0) {
+        currentIndex = totalImages - 1; // Regresar a la última imagen
+    } else if (currentIndex >= totalImages) {
+        currentIndex = 0; // Volver a la primera imagen
+    }
+
+    // Mover el carrusel a la posición correcta (desplazar las imágenes)
+    carousel.style.transform = 'translateX(' + (-currentIndex * 100) + '%)';
+}
+
 // Detección de dispositivo usando el Agente de Usuario
 function detectDevice() {
-    // Obtener la cadena del agente de usuario
     var userAgent = navigator.userAgent;
-
-    // Verificar si el dispositivo es móvil
     var isMobile = /iPhone|iPad|iPod|Android/i.test(userAgent);
 
     if (isMobile) {
-        // Móvil: Mostrar enlace de descarga
         document.getElementById("mobile-download").style.display = "block";
         document.getElementById("pc-download").style.display = "none";
     } else {
-        // PC: Mostrar QR
         document.getElementById("mobile-download").style.display = "none";
         document.getElementById("pc-download").style.display = "block";
-
-        // Generar QR
         generateQRCode();
     }
 }
@@ -23,25 +37,19 @@ function detectDevice() {
 // Generar el QR con la URL de la APK usando la API pública
 function generateQRCode() {
     var qrContainer = document.getElementById('qr-code');
-    var apkUrl = 'https://github.com/Frvncoo69/app_final/releases/download/1.0/tecnostore.apk'; // Cambia esta URL a tu enlace APK
-
-    // Usar la API pública QRServer para generar el QR
+    var apkUrl = 'https://github.com/Frvncoo69/app_final/releases/download/1.0/tecnostore.apk';
     var qrImageUrl = "https://api.qrserver.com/v1/create-qr-code/?data=" + encodeURIComponent(apkUrl) + "&size=150x150";
 
-    // Crear una imagen con el código QR generado
     var img = document.createElement("img");
     img.src = qrImageUrl;
     img.alt = "QR Code para descargar la app";
 
-    // Insertar la imagen en el contenedor del QR
     qrContainer.innerHTML = "";
     qrContainer.appendChild(img);
 }
 
 // Ejecutar la detección cuando la página cargue
 window.onload = function () {
-    detectDevice(); // Ejecuta la detección al cargar la página
-
-    // Re-ejecutar cuando se cambie el tamaño de la ventana
+    detectDevice();
     window.onresize = detectDevice;
 };
